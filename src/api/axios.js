04d18c24ca1cbb3d91e0b48df4e3b1a03fc6eaf4
XA1626 <132309726+XA1626@users.noreply.github.com>
2025-05-01 12:14:56 +1200

    v.0.0.0

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,5 +1,10 @@
 import axios from 'axios';
 
 export default axios.create({
+<<<<<<< Updated upstream
   baseURL: ' https://4ece37e22a38.ngrok.io',
+=======
+  baseURL: 'https://dfa9-156-62-117-17.ngrok-free.app',
+  // baseURL: 'http://localhost:3000',
+>>>>>>> Stashed changes
 });
